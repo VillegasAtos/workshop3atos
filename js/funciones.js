cbb579155345a27998de17400e2b4e4b6ac2fa22
Villegas545 <villegas_545@hotmail.com>
2021-12-10 21:47:04 -0700

    workshop3 point2 ready

--- js/funciones.js
+++ js/funciones.js
@@ -2,6 +2,12 @@ let count=0;
 $("#addButton").click(function(){
   if(count<2){
   count++;
-  $("#root").append(`<button>otroboton</button>`); 
+  $("#root").append(`<button id="btn${count}">otroboton</button>`); 
   }
+});
+
+$("#root").on("dblclick","#btn1",function(){
+  let color=`rgb(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)})`;
+  console.log(color);
+  $("body").css('background-color', color);
 });
